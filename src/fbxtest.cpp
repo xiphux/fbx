fb08795dea799f9b1ee44f7e32e011d1f0bf6ae1
xiphux <xiphux@terminaldogma.(none)>
2007-02-15 23:24:08 -0500

    Add test program

---
+++ src/fbxtest.cpp
@@ -0,0 +1,51 @@
+/*
+ * fbxtest.cpp
+ * FBX test program
+ * Copyright (C) 2007 Christopher Han
+ */
+
+#include <iostream>
+#include <string>
+#include "audio/AudioFactory.h"
+#include "audiofile/AudioFileFactory.h"
+
+#define BUFSIZE 2048
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		std::cout << "Requires an argument" << std::endl;
+		return 1;
+	}
+	fbx::AudioBase *audio = fbx::AudioFactory::CreateAudio(FBX_AUDIO_AO);
+	if (!audio) {
+		std::cout << "Could not create audio output" << std::endl;
+		return 2;
+	}
+	std::string fname = argv[1];
+	if (!fbx::AudioFileFactory::IsAudioFile(fname)) {
+		std::cout << "Not a recognized audio file" << std::endl;
+		return 2;
+	}
+	fbx::AudioFileBase *audiofile = fbx::AudioFileFactory::OpenAudioFile(fname);
+	if (!audiofile) {
+		std::cout << "Error opening audiofile" << std::endl;
+		return 2;
+	}
+	char buf[BUFSIZE];
+	int len = 0;
+	while (!audiofile->Eof()) {
+		len = audiofile->Read(buf, BUFSIZE);
+		if (len < 0) {
+			std::cout << "Error reading audiofile" << std::endl;
+			break;
+		} else {
+			std::cout << "Read: " << len << std::endl;
+			len = audio->Write(buf, len);
+			std::cout << "Wrote: " << len << std::endl;
+		}
+	}
+	delete audiofile;
+	delete audio;
+	return 0;
+}
